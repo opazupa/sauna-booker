083d8447bf255d7b20c3e5309c90225402fd18b6
Olli Saarinen <>
2021-09-27 10:53:04 +0300

    More cleanup

--- app/utils/puppeteer.ts
+++ app/utils/puppeteer.ts
@@ -1,10 +1,16 @@
 /* eslint-disable @typescript-eslint/ban-types */
 import chrome from 'chrome-aws-lambda';
-import puppeteer, { Page } from 'puppeteer-core';
+import puppeteer, { Browser, Page } from 'puppeteer-core';
 
 import { IConfiguration } from '../configuration';
 
-export const setup = async (config: IConfiguration) => {
+/**
+ * Setup puppeteer broser and page
+ *
+ * @param {IConfiguration} config
+ * @returns {Promise<{ browser: Browser; page: Page }>}
+ */
+export const setup = async (config: IConfiguration): Promise<{ browser: Browser; page: Page }> => {
   const browser = await puppeteer.launch({
     executablePath: config.isDev() ? config.chromeExecPath : await chrome.executablePath,
     args: config.isDev() ? ['--no-sandboxs'] : chrome.args,
@@ -13,17 +19,30 @@ export const setup = async (config: IConfiguration) => {
     slowMo: config.isDev() ? 250 : null,
   });
   const page = await browser.newPage();
-  page.setDefaultNavigationTimeout(60000);
+  page.setDefaultTimeout(60000);
   return { browser, page };
 };
 
+/**
+ * Keep short pause while executing
+ *
+ * @param {Page} page
+ */
 export const shortPause = async (page: Page) => {
   const pauseTime = 5000;
   await page.waitForTimeout(pauseTime);
 };
 
+/**
+ * Handle repeat click
+ *
+ * @param {Page} page
+ * @param {string} selector
+ * @param {number} times
+ */
 export const repeatClick = async (page: Page, selector: string, times: number) => {
-  for (let index = 0; index < times; index++) {
-    await page.click(selector);
+  for (let i = 0; i < times; i++) {
+    await page.waitForTimeout(500);
+    await page.click(selector).then(() => console.log(`Clicked ${selector} on repeat ${i + 1}/${times}`));
   }
 };
