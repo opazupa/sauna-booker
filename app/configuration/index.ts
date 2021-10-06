d7e0e33bcc0f6175c0857c51f8470c9f339c9a76
Olli Saarinen <>
2021-10-06 16:04:42 +0300

    Split sauna pref configuration

--- app/configuration/index.ts
+++ app/configuration/index.ts
@@ -1,56 +1,10 @@
 import * as dotenv from 'dotenv';
 
+import { DefaultSaunaPreferences, SaunaPreferences } from './sauna-preference';
+
+export { SaunaDay } from './sauna-preference';
+
 const DEVELOPMENT = 'dev';
-
-// Weekday
-export type Weekday = 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri' | 'Sat' | 'Sun';
-// Sauna time preference
-export type TimePreference = 'FIRST' | 'MIDDLE' | 'LAST';
-
-/**
- * Sauna day
- */
-export type SaunaDay = {
-  // Time of day
-  time: TimePreference;
-  // Two bookings on same day
-  double?: boolean;
-};
-/**
- * Sauna preferences
- */
-export type SaunaPreferences = {
-  [key in Weekday]?: SaunaDay;
-};
-
-/**
- * Default sauna preferences
- */
-export const DefaultSaunaPreferences: SaunaPreferences = {
-  // Only two should be enabled due sauna booking limit per week 2
-  // Mon: {
-  //   time: 'FIRST',
-  //   double: true,
-  // },
-  // Tue: {
-  //   time: 'LAST',
-  // },
-  // Wed: {
-  //   time: 'LAST',
-  // },
-  Thu: {
-    time: 'LAST',
-  },
-  Fri: {
-    time: 'LAST',
-  },
-  // Sat: {
-  //   time: 'MIDDLE',
-  // },
-  Sun: {
-    time: 'LAST',
-  },
-};
 
 /**
  * Env configuration interface
---
+++ app/configuration/sauna-preference.ts
@@ -0,0 +1,49 @@
+// Weekday
+export type Weekday = 'Mon' | 'Tue' | 'Wed' | 'Thu' | 'Fri' | 'Sat' | 'Sun';
+// Sauna time preference
+export type TimePreference = 'FIRST' | 'MIDDLE' | 'LAST';
+
+/**
+ * Sauna day
+ */
+export type SaunaDay = {
+  // Time of day
+  time: TimePreference;
+  // Two bookings on same day
+  double?: boolean;
+};
+/**
+ * Sauna preferences
+ */
+export type SaunaPreferences = {
+  [key in Weekday]?: SaunaDay;
+};
+
+/**
+ * Default sauna preferences
+ */
+export const DefaultSaunaPreferences: SaunaPreferences = {
+  // Only two should be enabled due sauna booking limit per week 2
+  // Mon: {
+  //   time: 'FIRST',
+  //   double: true,
+  // },
+  // Tue: {
+  //   time: 'LAST',
+  // },
+  // Wed: {
+  //   time: 'LAST',
+  // },
+  // Thu: {
+  //   time: 'LAST',
+  // },
+  Fri: {
+    time: 'LAST',
+  },
+  // Sat: {
+  //   time: 'MIDDLE',
+  // },
+  Sun: {
+    time: 'LAST',
+  },
+};
